3b3e0b4a6391700e8928d1ccce456c81951b963d
Ricard Larios <ricard.larios@gmail.com>
2024-10-07 08:37:15 +0200

    improve menuoper

--- roles/my_role/files/check_environment.sh
+++ roles/my_role/files/check_environment.sh
@@ -1,19 +1,86 @@
 #!/bin/bash
 
-# Definir colores
-verde='\033[0;32m'
-rojo='\033[0;31m'
-sin_color='\033[0m'  # Restablecer al color por defecto
+# Set the threshold percentage
+THRESHOLD=95
 
-# Define the URL to check
-URL="http://localhost"
+echo ""
+echo "-------------------------------------------------------"
+echo "Checking resources above THRESHOLD: ${THRESHOLD}%"
+echo "-------------------------------------------------------"
+echo ""
 
-# Use curl to get the response body from the URL
-response=$(curl -s $URL)
+# ANSI color codes
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m'  # No Color
 
-# Check if the response contains the string "Managed by Ansible"
-if [[ "$response" == *"Managed by Ansible"* ]]; then
-    echo -e "${verde}Check application is OK.${sin_color}"
+# Check block usage (excluding memory filesystems like tmpfs, devtmpfs)
+echo "Checking filesystem block usage..."
+df -hP | grep -vE '^tmpfs|^devtmpfs' | awk 'NR>1 {print $5 " " $6}' | while read output;
+do
+  usage=$(echo $output | awk '{print $1}' | sed 's/%//')
+  partition=$(echo $output | awk '{print $2}')
+
+  if [ $usage -ge $THRESHOLD ]; then
+    echo -e "${RED}WARNING: Filesystem $partition is at $usage% block usage!${NC}"
+  else
+    echo -e "${GREEN}OK: Filesystem $partition is at $usage% block usage.${NC}"
+  fi
+done
+echo " ---- "
+
+# Check inode usage (excluding memory filesystems like tmpfs, devtmpfs)
+echo "Checking filesystem inode usage..."
+df -hiP | grep -vE '^tmpfs|^devtmpfs|^shm' | awk 'NR>1 {print $5 " " $6}' | while read output;
+do
+  usage=$(echo $output | awk '{print $1}' | sed 's/%//')
+  partition=$(echo $output | awk '{print $2}')
+
+  if [ $usage -ge $THRESHOLD ]; then
+    echo -e "${RED}WARNING: Filesystem $partition is at $usage% inode usage!${NC}"
+  else
+    echo -e "${GREEN}OK: Filesystem $partition is at $usage% inode usage.${NC}"
+  fi
+done
+
+echo " ---- "
+
+# Check CPU usage
+echo  "Checking CPU usage..."
+cpu_usage=$(top -bn1 | grep "Cpu(s)" | awk '{print $2 + $4}')
+cpu_usage=${cpu_usage%.*}  # Convert to integer for comparison
+
+if [ $cpu_usage -ge $THRESHOLD ]; then
+  echo -e "${RED}WARNING: CPU usage is at $cpu_usage%!${NC}"
 else
-    echo -e "${rojo}ERROR: Check application failed!!.${sin_color}"
+  echo -e "${GREEN}OK: CPU usage is at $cpu_usage%.${NC}"
 fi
+
+echo " ---- "
+
+# Check memory usage
+echo  "Checking memory usage..."
+memory_usage=$(free | grep Mem | awk '{print $3/$2 * 100.0}')
+memory_usage=${memory_usage%.*}  # Convert to integer for comparison
+
+if [ $memory_usage -ge $THRESHOLD ]; then
+  echo -e "${RED}WARNING: Memory usage is at $memory_usage%!${NC}"
+else
+  echo -e "${GREEN}OK: Memory usage is at $memory_usage%.${NC}"
+fi
+
+
+echo " ---- "
+
+# Check swap usage
+echo  "Checking swap usage..."
+swap_usage=$(free | grep Swap | awk '{if ($2 > 0) print $3/$2 * 100.0; else print 0}')
+swap_usage=${swap_usage%.*}  # Convert to integer for comparison
+
+if [ $swap_usage -ge $THRESHOLD ]; then
+  echo -e "${RED}WARNING: Swap usage is at $swap_usage%!${NC}"
+else
+  echo -e "${GREEN}OK: Swap usage is at $swap_usage%.${NC}"
+fi
+
+echo " ---- "
---
+++ roles/my_role/files/check_url.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Definir colores
+verde='\033[0;32m'
+rojo='\033[0;31m'
+sin_color='\033[0m'  # Restablecer al color por defecto
+
+# Define the URL to check
+URL="http://localhost:3000"
+
+# Use curl to get the response body from the URL
+response=$(curl -s $URL)
+
+# Check if the response contains the string "Managed by Ansible"
+if [[ "$response" == *"Managed by Ansible"* ]]; then
+    echo -e "${verde}Check application is OK.${sin_color}"
+else
+    echo -e "${rojo}ERROR: Check application failed!!.${sin_color}"
+fi
